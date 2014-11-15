a21a0231fb550c02a65dcae0fe9c85006ef33e70
Jake xz <jakexzwork@gmail.com>
2014-11-15 12:47:50 -0800

    new compile

--- DiscreteSort/src/discretesort/DiscreteSort.java
+++ DiscreteSort/src/discretesort/DiscreteSort.java
@@ -17,14 +17,6 @@ public class DiscreteSort {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    	   	
-    	// Begin - Section for Algorithm 6 -Greedy Change Maker 
-		String[] coins = { ".25", ".10", ".05", ".01" }; // Coin denominations
-		String[] value = { "0.81" };                     // $ amount to convert into coins		
-		String displayResult = greedyChangeMaker(coins, value);
-		// Temporary output display
-		System.out.println(displayResult);
-		// End - Section for Algorithm 6 -Greedy Change Maker 
 		
        ArrayGen a1 = new ArrayGen(); 
        Object[] array = a1.getArray(5,'i');// TODO code application logic here
@@ -32,32 +24,5 @@ public class DiscreteSort {
        disp.display(array);
        
     }
-    
-    // Algorithm 6 - Greedy Change Maker
-    protected static String greedyChangeMaker(String[] coinTypeArray, String[] totalChangeAmt) {
-		String result = "";
-		int sameCoinQuantity = 0;
-		// coin value times 100 to work around precision problem of 'double' datatype
-		double totalChangeAmount = Double.parseDouble(totalChangeAmt[0]) * 100;
-		double workingBalance = totalChangeAmount;
-		double coinType;
-
-		for (int i = 0; i < coinTypeArray.length; i++) {
-			// conversion of array string value to double x 100
-			coinType = Double.parseDouble(coinTypeArray[i]) * 100;
-
-			while (workingBalance >= coinType) {
-				// same coin-type quantity counter
-				sameCoinQuantity++;
-				// decrease balance with each coin type
-				workingBalance -= coinType;
-			}
-			// build string to record coin-type and coin-quantity
-			result += coinTypeArray[i] + ": " + sameCoinQuantity + "    ";
-			// reset same coin-type quantity counter
-			sameCoinQuantity = 0;		
-		}
-		return result;
-	}
     
 }
